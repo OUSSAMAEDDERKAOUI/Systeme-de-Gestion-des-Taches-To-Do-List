d0d08660abf291e54b03b057b65d63b393ef0d86
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2024-11-03 12:33:45 +0100

    L'ajoute de modification sur la structure de java script : utilisation des objets .

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,11 @@
 
-
-
+//creation de principale obejet
+const tasks = {
+    todo: [],
+    doing: [],
+    done: [],
+};
+// ouvrir et fermer les popup
 document.getElementById('openPopupBtn').onclick = function () {
     document.getElementById('popup').classList.remove('hidden');
 };
@@ -15,108 +20,157 @@ window.onclick = function (event) {
         popup.classList.add('hidden');
     }
 };
-
+//creation de variable pour stocke les input de form popup
 document.getElementById('addTaskBtn').onclick = function () {
-    const title = document.getElementById('TitleInput').value;
-    const description = document.getElementById('descriptionInput').value;
+    const title = document.getElementById("TitleInput").value;
+    const description = document.getElementById("descriptionInput").value;
+    const priority = document.getElementById("PriorityInput").value;
+    const dueDate = document.getElementById("DateInput").value;
+    const taskStatus = document.getElementById("taskStatusInput").value;
 
-    const priority = document.getElementById('PriorityInput').value;
-    const dueDate = document.getElementById('DateInput').value;
-    const taskStatus = document.getElementById('taskStatusInput').value;
+
+    if (title && description && priority && dueDate && taskStatus) {
+        const taskItem = {
+            title,
+            description,
+            priority,
+            dueDate,
+            taskStatus,
+        };
+        // creation de div pour stocke en div de  (todo ou doing ou done )
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('border', 'border-gray-300', 'rounded', 'p-2', 'mb-2', 'bg-gray-50');
+        taskElement.innerHTML = `
+        <h1 > <span class=" font-extrabold">Title :</span> ${taskItem.title}</h1>
+        <div class=" desc-Toggel hidden"> <span class="font-bold">description :</span>${taskItem.description}</div>
+        <div class="font-bold  "> Priorité :  <span class="priorityClass">${taskItem.priority}</span> , Échéance :  <sapan class="dateClass">${taskItem.dueDate}</sapan></div>
+         <button class="editBtn bg-blue-500 text-white rounded px-2 py-1 mr-2">Modifier</button>                
+         <button class="deleteBtn bg-red-500 text-white rounded px-2 py-1">Supprimer</button>
+        `
+        // afficher les descriptions en leur place avec toggele
+        taskElement.onclick = function () {
+            const descAffichage = taskElement.querySelector('.desc-Toggel');
+            descAffichage.classList.toggle('hidden');
+        };
+        //stocker les taskElement en leur etat automatiquement
+        if (taskStatus === "todo") {
+            tasks.todo.push(taskItem);
+            document.getElementById("todoList").appendChild(taskElement);
 
 
-    if (title && description && priority && dueDate) {
-        // Créer un nouvel élément de tâche
-        const taskItem = document.createElement('li');
-        taskItem.className = '  flex flex-col justify-between items-start p-4 bg-white rounded mb-2 shadow';
-
-        taskItem.innerHTML = `
-            <div>
-                <h3 class="font-bold">${title}</h3>
-                <div class=' desc hidden ' >${description} </div>
-                <div>Priorité : ${priority}, Échéance: ${dueDate}</div>
-            </div>
-            <div class="mt-2">
-                <button class="editBtn bg-blue-500 text-white rounded px-2 py-1 mr-2">Modifier</button>
-                <button class="deleteBtn bg-red-500 text-white rounded px-2 py-1">Supprimer</button>
-                <button class="aficherDesc bg-green-500 text-white rounded px-2 py-1">plus</button>
-
-            </div>
-        `;
-
-        // Ajouter la tâche 
-        if (taskStatus === 'todo') {
-            document.getElementById('todoList').appendChild(taskItem);
         }
-        else if (taskStatus === 'doing') {
-            document.getElementById('doingList').appendChild(taskItem);
-        }
-        else {
-            document.getElementById('doneList').appendChild(taskItem);
+        else if (taskStatus === "doing") {
+            tasks.doing.push(taskItem);
+            document.getElementById("doingList").appendChild(taskElement);
+
 
         }
-        // Ajouter la couleur pour chaque priority 
+        else if (taskStatus === "done") {
+            tasks.done.push(taskItem);
+            document.getElementById("doneList").appendChild(taskElement);
+
+        }
+        // colorer les taskElements selan leur priorite
         if (priority === 'P1') {
-            taskItem.style.borderLeft = '4px double red';
+            taskElement.style.borderLeft = '4px double red';
 
         }
-        else if (priority === 'P2') {
-            taskItem.style.borderLeft = '4px double orange';
-        }
-        else {
-            taskItem.style.borderLeft = '4px double green';
+        if (priority === 'P2') {
+            taskElement.style.borderLeft = '4px double orange';
+
+        } if (priority === 'P3') {
+            taskElement.style.borderLeft = '4px double green';
 
         }
-        // afficher la discription  
+        count();
+        // la fonction de suppression 
+        taskElement.querySelector('.deleteBtn').onclick = function (e) {
+            e.stopPropagation();
+            const confirmDelete = confirm("Voulez-vous vraiment supprimer cette tache ?");
+            if (confirmDelete) {
+                taskElement.remove();
+                count();
+            }
 
-        taskItem.querySelector('.aficherDesc').onclick = function () {
-            const descElement = taskItem.querySelector('.desc');
-            descElement.classList.toggle('hidden');
         };
+        // //fonction de la modification 
+        document.getElementById('popupEdit').classList.add('hidden');
+
+        taskElement.querySelector('.editBtn').onclick = function () {
+
+            document.getElementById('popupEdit').classList.remove('hidden');
+
+            const newTitle = taskElement.querySelector('h1');
+            const newDescription = taskElement.querySelector('.desc-Toggel');
+            const newPriority = taskElement.querySelector('.priorityClass');
+            const newDate = taskElement.querySelector('.dateClass');
+
+            document.getElementById('TitleInputEdit').value = newTitle.textContent;
+            document.getElementById('descriptionInputEdit').value = newDescription.textContent;
+            document.getElementById('PriorityInputEdit').value = newPriority.textContent;
+            document.getElementById('DateInputEdit').value = newDate.textContent;
+            document.getElementById('taskStatusInputEdit').value = taskElement.parentElement.dataset.list;
 
 
-        document.getElementById('TitleInput').value = '';
-        document.getElementById('descriptionInput').value = '';
-        document.getElementById('PriorityInput').value = '';
-        document.getElementById('DateInput').value = '';
+            document.getElementById('addTaskBtnEdit').onclick = function (e) {
+                e.preventDefault();
+                const titleElement = document.getElementById('TitleInputEdit').value;
+                const descriptionElement = document.getElementById('descriptionInputEdit').value;
+                const priorityElement = document.getElementById('PriorityInputEdit').value;
+                const dateElement = document.getElementById('DateInputEdit').value;
+                const StatusElement = document.getElementById('taskStatusInputEdit').value;
+                // Mettre à jour les éléments de la tâche:
+                newTitle.textContent = `${titleElement}`;
+                newDescription.innerHTML = `${descriptionElement}`;
+                newPriority.innerHTML = `${priorityElement}`;
+                newDate.innerHTML = `${dateElement}`;
+                
+        
+                count();
+                if (StatusElement === 'todo') {
+                    tasks.todo.push(taskItem);
+                    document.getElementById("todoList").appendChild(taskElement);
+                }
+                else if (StatusElement === 'doing') {
+                    tasks.doing.push(taskItem);
+                    document.getElementById("doingList").appendChild(taskElement);
+                }
+                else if (StatusElement === 'done') {
+                    tasks.done.push(taskItem);
+                    document.getElementById("doneList").appendChild(taskElement);
+                }
+                // Ajouter la couleur pour chaque priority 
+                if (priorityElement === 'P1') {
+                    taskElement.style.borderLeft = '4px double red';
 
-        document.getElementById('popup').classList.add('hidden');
+                }
+                else if (priorityElement === 'P2') {
+                    taskElement.style.borderLeft = '4px double orange';
+                }
+                else {
+                    taskElement.style.borderLeft = '4px double green';
 
-        taskItem.querySelector('.editBtn').onclick = function () {
-            const task = this.closest('li');
-            const titleElement = task.querySelector('h3');
-            const descriptionElement = task.querySelector('div');
+                }
+                document.getElementById('popupEdit').classList.add('hidden');
 
-            document.getElementById('TitleInput').value = titleElement.textContent;
-            document.getElementById('descriptionInput').value = descriptionElement.textContent.split(' (')[0];
-            document.getElementById('popup').classList.remove('hidden');
 
-            document.getElementById('addTaskBtn').onclick = function () {
-                const newTitle = document.getElementById('TitleInput').value;
-                const newDescription = document.getElementById('descriptionInput').value;
-                const newPriority = document.getElementById('PriorityInput').value;
-                const newDueDate = document.getElementById('DateInput').value;
-
-                // Mettre à jour les éléments de la tâche
-                titleElement.textContent = newTitle;
-                descriptionElement.innerHTML = `${newDescription} (Priorité: ${newPriority}, Échéance: ${newDueDate})`;
-
-                // Réinitialiser les champs
-                document.getElementById('TitleInput').value = '';
-                document.getElementById('descriptionInput').value = '';
-                document.getElementById('PriorityInput').value = '';
-                document.getElementById('DateInput').value = '';
-
-                // Cacher la popup
-                document.getElementById('popup').classList.add('hidden');
+            }
+            // fermer la popup
+            document.getElementById('closeBtnEdit').onclick = function () {
+                document.getElementById('popupEdit').classList.add('hidden');
             };
-        };
-
-        taskItem.querySelector('.deleteBtn').onclick = function () {
-            const task = this.closest('li');
-            task.remove(); // Supprimer la tâche de la liste
-        };
-    } else {
+        }
+    }
+    else {
         alert('Veuillez remplir tous les champs.');
     }
-};
+
+}
+function count() {
+    document.getElementById('todocount').textContent = document.getElementById('todoList').childElementCount;
+    document.getElementById('doingcount').textContent = document.getElementById('doingList').childElementCount;
+
+    document.getElementById('donecount').textContent = document.getElementById('doneList').childElementCount;
+
+
+}
